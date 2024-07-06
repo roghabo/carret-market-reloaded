138705a54b154d112d826b820e575b2c662e5c39
Jinyoung <roghabo@gmail.com>
2024-07-06 17:23:59 +0900

    finish authentication UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,24 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col">
-        <div className="group flex flex-col">
-          <input
-            className="bg-gray-100 w-full"
-            placeholder="Write your email"
-          />
-          <span className="group-focus-within:block hidden">
-            Make sure it is a valied email...
-          </span>
-          <button>Submit</button>
+    <div className="flex flex-col items-center justify-between min-h-screen p-6">
+      <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
+        <span className="text-9xl">🥕</span>
+        <h1 className="text-4xl">당근</h1>
+        <h2 className="text-2xl">당근 마켓에 오서오세요!</h2>
+      </div>
+      <div className="flex flex-col items-center gap-3 w-full">
+        <Link href="/create-account" className="primary-btn text-lg py-2.5">
+          시작하기
+        </Link>
+        <div className="flex gap-2">
+          <span>이미 계정이 있나요?</span>
+          <Link href="/login" className="hover:underline">
+            로그인
+          </Link>
         </div>
       </div>
-    </main>
+    </div>
   );
 }
