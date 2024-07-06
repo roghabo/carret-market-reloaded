b5b4bc09007f138d1c9d3abdbb66b1e3afceac12
Jinyoung <roghabo@gmail.com>
2024-07-06 13:18:38 +0900

    list tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,22 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2 *:outline-none ring ring-transparent transition-shadow has-[:invalid]:ring-red-100">
-        <input
-          className="w-full rounded-full h-12 pl-5 bg-gray-200 ring ring-transparent focus:ring-blue-500 focus:ring-offset-2 transition-shadow invalid:focus:ring-red-500 peer"
-          type="text"
-          required
-          placeholder="Search here..."
-        />
-        <span className="text-red-500 font-medium hidden peer-invalid:block">
-          Email is required
-        </span>
-        <button className=" text-white py-2 rounded-full active:scale-90 transition font-medium md:px-10 bg-gradient-to-tr from-cyan-500 via-yellow-400 to-purple-400 peer-invalid:from-red-500 peer-invalid:to-red-500">
-          Search
-        </button>
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col">
+        {["Nico", "Me", "You", "Yourself"].map((person, index) => (
+          <div
+            key={index}
+            className="flex items-center group gap-3 p-2.5 rounded-xl"
+          >
+            <div className="size-10 bg-blue-400 rounded-full" />
+            <span className="text-lg font-medium empty:w-24 empty:h-5 empty:rounded-full empty:animate-pulse empty:bg-gray-300 group-hover:text-red-500">
+              {person}
+            </span>
+            <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full relative">
+              <span className="z-10">{index}</span>
+              <div className="size-6 bg-red-500 rounded-full absolute animate-ping" />
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );
