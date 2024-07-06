d0f5ee8a4527098fc3545c43dd3b83e52a2c5188
Jinyoung <roghabo@gmail.com>
2024-07-06 12:36:03 +0900

    tailwind card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,19 @@
 export default function Home() {
   return (
-    <main className="bg-gray-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm dark:bg-gray-600">
-        <div className="flex justify-between items-center">
-          <div className="flex flex-col">
-            <span className="text-gray-600 font-semibold -mb-1 dark:text-gray-300">
-              In transit
-            </span>
-            <span className="font-4xl font-semibold dark:text-white">
-              Coolblue
-            </span>
-          </div>
-          <div className="size-12 rounded-full bg-orange-400" />
-        </div>
-        <div className="my-2 flex items-center gap-2">
-          <span className="bg-green-400 text-white uppercase px-2.5 py-1.5 text-xs font-medium rounded-full hover:bg-green-500 hover:scale-125 transition">
-            Today
-          </span>
-          <span className="dark:text-gray-100">9:30-10:30u</span>
-        </div>
-        <div className="relative">
-          <div className="bg-gray-200 absolute rounded-full w-full h-2" />
-          <div className="bg-green-400 absolute rounded-full w-2/3 h-2" />
-        </div>
-        <div className="flex justify-between items-center mt-5 text-gray-600 dark:text-gray-300">
-          <span>Expected</span>
-          <span>Sorting center</span>
-          <span>In transit</span>
-          <span className="text-gray-400 dark:text-gray-500">Delivered</span>
-        </div>
+    <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2 *:outline-none ring ring-transparent transition-shadow has-[:invalid]:ring-red-100">
+        <input
+          className="w-full rounded-full h-12 pl-5 bg-gray-200 ring ring-transparent focus:ring-blue-500 focus:ring-offset-2 transition-shadow invalid:focus:ring-red-500 peer"
+          type="text"
+          required
+          placeholder="Search here..."
+        />
+        <span className="text-red-500 font-medium hidden peer-invalid:block">
+          Email is required
+        </span>
+        <button className=" text-white py-2 rounded-full active:scale-90 transition font-medium md:px-10 bg-gradient-to-tr from-cyan-500 via-yellow-400 to-purple-400 peer-invalid:from-red-500 peer-invalid:to-red-500">
+          Search
+        </button>
       </div>
     </main>
   );
