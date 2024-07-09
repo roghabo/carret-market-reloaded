1087d63152541e7eccc18e33e9fd2c50ba819e0f
Jinyoung <roghabo@gmail.com>
2024-07-09 10:44:51 +0900

    finish authentication

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,11 +4,11 @@ import Button from "@/components/button";
 import Input from "@/components/input";
 import SocialLogin from "@/components/social-login";
 import { useFormState } from "react-dom";
-import { login } from "./actions";
+import { logIn } from "./actions";
 import { PASSWORD_MIN_LENGTH } from "@/lib/constants";
 
 export default function LogIn() {
-  const [state, dispatch] = useFormState(login, null);
+  const [state, dispatch] = useFormState(logIn, null);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
